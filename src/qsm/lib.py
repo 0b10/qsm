3b3ffd6911e48f3458c4a9f5e48818e471311d55
0b10 <0b10@pm.me>
2019-12-01 12:11:10 +0000

    Create set_prefs()

--- src/qsm/lib.py
+++ src/qsm/lib.py
@@ -3,9 +3,20 @@ from qsm.error import raise_process_error
 
 
 def create(name, options=None):
+    print("creating vm: {}".format(name))
     _options = "\b" if options is None else options
     try:
         check_call("qvm-create {} {}".format(_options, name), shell=True)
     except CalledProcessError:
         raise_process_error()
 
+
+def set_prefs(target, options):
+    print("setting prefs for {}:".format(target))
+    try:
+        for _option, _value in options.items():
+            print(">>> {}: {}".format(_option, _value))
+            check_call("qvm-prefs -s {} {} \'{}\'".format(target,
+                                                          _option, _value), shell=True)
+    except CalledProcessError:
+        raise_process_error("- cannot set vm preferences")
