ec892d8b5ca588a6885e7a6814ac237e20462e2f
0b10 <0b10@pm.me>
2019-12-01 13:06:22 +0000

    Make printing functions
       ...so that messages are better formatted, and have colour

--- src/qsm/lib.py
+++ src/qsm/lib.py
@@ -1,21 +1,19 @@
 from subprocess import check_call, CalledProcessError
 from qsm.error import raise_process_error
-from qsm.constants import GREEN, WHITE
+from qsm.constants import GREEN, WHITE, RED
 
 
-def cprint(message, sub=False, colour=WHITE):
-    if sub:
-        print(colour + ">>> " + message + WHITE)
-    else:
-        print(colour + message + WHITE)
+def print_header(message):
+    print(WHITE + message + "...")
 
 
-def print_success(message):
-    cprint(message, sub=True, colour=GREEN)
+def print_sub(message, failed=False):
+    _colour = RED if failed else GREEN
+    print(_colour + ">>> " + message + WHITE)
 
 
 def create(name, label, options=None):
-    cprint("creating vm: {}".format(name))
+    print_header("creating vm {}".format(name))
 
     _options = "\b" if options is None else options
     try:
@@ -24,34 +22,34 @@ def create(name, label, options=None):
     except CalledProcessError:
         raise_process_error()
 
-    print_success("{} created".format(name))
+    print_sub("{} created".format(name))
 
 
 def set_prefs(target, options):
-    cprint("setting prefs for {}:".format(target))
+    print_header("setting prefs for {}".format(target))
 
     try:
         for _option, _value in options.items():
             check_call("qvm-prefs -s {} {} \'{}\'".format(target,
                                                           _option, _value), shell=True)
-            print_success("{}: {}".format(_option, _value))
+            print_sub("{}: {}".format(_option, _value))
     except CalledProcessError:
         raise_process_error("- cannot set vm preferences")
 
 
 def start(target):
-    cprint("starting {}...".format(target))
+    print_header("starting {}".format(target))
 
     try:
         check_call("qvm-start --skip-if-running {}".format(target), shell=True)
     except CalledProcessError:
         raise_process_error("- unable to start vm")
 
-    print_success("{} started".format(target))
+    print_sub("{} started".format(target))
 
 
 def stop(target, timeout=120):
-    cprint("stopping {}...".format(target))
+    print_header("stopping {}".format(target))
 
     try:
         check_call(
@@ -59,26 +57,26 @@ def stop(target, timeout=120):
     except CalledProcessError:
         raise_process_error("- unable to stop vm")
 
-    print_success("{} stopped".format(target))
+    print_sub("{} stopped".format(target))
 
 
 def remove(target):
-    cprint("removing {}...".format(target))
+    print_header("removing {}".format(target))
 
     try:
         check_call("qvm-remove --force {}".format(target), shell=True)
     except CalledProcessError:
         raise_process_error("- unable to remove vm")
 
-    print_success("{} removed".format(target))
+    print_sub("{} removed".format(target))
 
 
 def clone(source, target):
-    cprint("cloning {} into {}...".format(source, target))
+    print_header("cloning {} into {}".format(source, target))
 
     try:
         check_call("qvm-clone {} {}".format(source, target), shell=True)
     except CalledProcessError:
         raise_process_error("- unable to remove vm")
 
-    print_success("{} created".format(target))
+    print_sub("{} created".format(target))
