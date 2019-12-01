f2ac110ac83a1e6f2d0daab72fe5f90e3efcec3f
0b10 <0b10@pm.me>
2019-12-01 02:47:09 +0000

    Fix failing template tests, and add more tests
       ...the tests are too complicated, each test is testing against a
          complicated regex, making it prone to failure. Refactor these
          tests so that each one is testing on aspect of the call

--- src/qsm/tests/test_template.py
+++ src/qsm/tests/test_template.py
@@ -6,24 +6,83 @@ import re
 def test_update_exists():
     assert update is not None, "should exist, but doesn't"
 
+# >>> UPDATE >>>
 
-def test_update_executes_update_script():
+
+def test_update_uses_qvm_run():
+    with patch("qsm.run.check_call", return_value=0, autospec=True) as mock_check_call:
+        update("fedora-template")
+        _arg = mock_check_call.call_args[0][0]
+
+        # \w\W any char including newline
+        assert re.search(r"^qvm-run ", _arg), "qvm-run was not used"
+
+
+def test_update_executes_dnf_update():
     with patch("qsm.run.check_call", return_value=0, autospec=True) as mock_check_call:
         update("fedora-template")
         _arg = mock_check_call.call_args[0][0]
 
         # \w\W any char including newline
         assert re.search(
-            r"^qvm-run --user root[\w\W]+fedora-template[\w\W]+dnf update -y[\w\W]+$", _arg), \
-            "qvm-run as root, and/or update script not executed"
+            r"dnf update[\w\W]+-y", _arg), "dnf is not executed"
 
 
-def test_install_executes_install_script():
+def test_update_executes_as_root():
+    with patch("qsm.run.check_call", return_value=0, autospec=True) as mock_check_call:
+        update("fedora-template")
+        _arg = mock_check_call.call_args[0][0]
+
+        # \w\W any char including newline
+        assert re.search(
+            r" --user root ", _arg), "update was not run as root"
+
+
+def test_update_excutes_on_correct_target():
+    with patch("qsm.run.check_call", return_value=0, autospec=True) as mock_check_call:
+        update("fedora-template")
+        _arg = mock_check_call.call_args[0][0]
+
+        assert re.search(
+            r" fedora-template ", _arg), "update not executed on the correct target"
+
+# >>> INSTALL >>>
+
+
+def test_install_uses_qvm_run():
     with patch("qsm.run.check_call", return_value=0, autospec=True) as mock_check_call:
         install("fedora-template", ["vim",  "nano"])
         _arg = mock_check_call.call_args[0][0]
 
         # \w\W any char including newline
         assert re.search(
-            r"^qvm-run --user root[\w\W]+fedora-template[\w\W]+dnf install -y[\w\W]+\'vim nano\' \'another_arg\'", _arg), \
-            "qvm-run as root, and/or install script not executed properly"
+            r"^qvm-run ", _arg), "qvm-run was not used"
+
+
+def test_install_executes_dnf_install():
+    with patch("qsm.run.check_call", return_value=0, autospec=True) as mock_check_call:
+        install("fedora-template", ["vim",  "nano"])
+        _arg = mock_check_call.call_args[0][0]
+
+        # \w\W any char including newline
+        assert re.search(
+            r"dnf install -y vim nano", _arg), "dnf is not executed"
+
+
+def test_install_executes_as_root():
+    with patch("qsm.run.check_call", return_value=0, autospec=True) as mock_check_call:
+        install("fedora-template", ["vim",  "nano"])
+        _arg = mock_check_call.call_args[0][0]
+
+        # \w\W any char including newline
+        assert re.search(
+            r" --user root ", _arg), "install was not run as root"
+
+
+def test_install_excutes_on_correct_target():
+    with patch("qsm.run.check_call", return_value=0, autospec=True) as mock_check_call:
+        install("fedora-template", ["vim",  "nano"])
+        _arg = mock_check_call.call_args[0][0]
+
+        assert re.search(
+            r" fedora-template ", _arg), "install not executed on the correct target"
