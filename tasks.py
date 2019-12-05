9b76805fbd49931691dcfd41992c171771f8b447
0b10 <0b10@pm.me>
2019-12-05 04:01:39 +0000

    Make autopep8 task

--- tasks.py
+++ tasks.py
@@ -10,3 +10,14 @@ def configure(ctx):
     """
 
     run("%s -m pip install .[dev] -r requirements.txt" % sys.executable)
+
+
+@task
+def pep8(ctx):
+    args = " ".join([
+        "--recursive",
+        "--in-place",
+        "--max-line-length 120",
+        "./src/"
+    ])
+    run("{} -m autopep8 {}".format(sys.executable, args))
