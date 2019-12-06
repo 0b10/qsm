3f0ddb2231851b8a8de304bc23e5071c4dc40fc6
0b10 <0b10@pm.me>
2019-12-06 08:36:19 +0000

    Inject conf_dir into config.init

--- src/qsm/config.py
+++ src/qsm/config.py
@@ -1,30 +1,32 @@
 from os.path import join, expanduser, isfile
 from os import makedirs, chmod
 import json
-
-_CONFIG_DIR = join(expanduser("~"), ".qsm")
-_CONFIG_FILE = join(_CONFIG_DIR, "qsm.conf")
+import lib
 
 
-def init():
-    pluging_dir = join(_CONFIG_DIR, "plugins")
-    data_dir = join(_CONFIG_DIR, "data")
+def init(config_dir=join(expanduser("~"), ".qsm")):
+    config_file = join(config_dir, "qsm.conf")
+    pluging_dir = join(config_dir, "plugins")
+    data_dir = join(config_dir, "data")
 
-    makedirs(_CONFIG_DIR, exist_ok=True, mode=0o750)
+    makedirs(config_dir, exist_ok=True, mode=0o750)
     makedirs(pluging_dir, exist_ok=True, mode=0o750)
     makedirs(data_dir, exist_ok=True, mode=0o750)
 
-    if not isfile(_CONFIG_FILE):
+    if not isfile(config_file):
+        lib.print_header("creating a new config file")
         # true for dirs, let raise if is dir
-        with open(_CONFIG_FILE, "w") as f:
+        with open(config_file, "w") as f:
             json.dump({
                 "data_dir": data_dir,
                 "plugin_dir": pluging_dir
             }, f, indent=2, sort_keys=True)
-            chmod(_CONFIG_FILE, 0o640)
+            chmod(config_file, 0o640)
+        lib.print_sub("config file created @ {}".format(config_file))
+
+    return config_file
 
 
 def config(option):
-    init()
-    with open(_CONFIG_FILE, "r") as f:
+    with open(init(), "r") as f:
         return json.load(f)[option]
